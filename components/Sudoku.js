85e132e4e04fd65fbb38671547500941d5f712c7
Yang SHEN <biztudio@outlook.com>
2019-03-04 17:18:38 +0800

    to code the sudoku

--- components/Sudoku.js
+++ components/Sudoku.js
@@ -1,9 +1,52 @@
-//import sudoku from 'sudokukit';
+import sudokukit from '../middleware/sudokukit';
 
-const Sudoku = () => (
-    <div>
-        Hello Sudoku!
-    </div>
-);
+const groupsize = 9;
+const getSudoku = level => {
+    
+    let puzzle = sudokukit.getSudokuPuzzle(level).SudokuPuzzle//level is between 2 and 6
+    let groupCount = puzzle.length / groupsize;
+    let sudokuGroups = [];
+   
+    for(let p of puzzle){
+        let cellsInGroup = [];
+        //TODO:
+        sudokuGroups.push(cellsInGroup.slice(0));
+    }
+    
+    return (
+        <div>
+            <div className='sudokucontainer'>
+                {puzzle.map(s => <div key={s.index}>{s.display}</div>)}          
+            </div>
+            <style jsx>{`
+                .sudokucontainer{
+                    display:flex;
+                    flex-direction:row;
+                    flex-wrap: wrap;
+                }
+                .sudokugroup{
+                    flex-wrap: wrap;
+                }
+                .sudokuanswer{
+                    color: purple;
+                }
+                .sudokuhint{
+                    color: gray;
+                }
+            `}</style>
+        </div>
+    );
+}
 
-export default Sudoku;
+const SudokuComponent = (props) => {
+    
+    return (
+        <div>
+            Hello, this is Sudoku!
+            {getSudoku(props.level)}
+           
+        </div>
+    )
+};
+
+export default SudokuComponent;
--- pages/sudoku.js
+++ pages/sudoku.js
@@ -1,12 +1,37 @@
 import Layout from '../components/Layout';
-import Sudoku from '../components/Sudoku';
+import SudokuComponent from '../components/Sudoku';
 
-export default () => (
+
+const Sudoku = (props) => (
     
     <Layout   title="Nextbrain - 数独">
 
-        <Sudoku />
+        <SudokuComponent level='3' />
 
     </Layout>
     
 )
+
+
+// getInitialProps works ONLY on page
+/*
+
+Sudoku.getInitialProps = async function() {
+    
+
+    //let temp_pool = [11,12,13,14,15,16,17,18];
+    //let index = mathkit.get_random_number_index(temp_pool.length);
+    //console.log(temp_pool[index]);
+
+    //const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    //const data = await res.json()
+    //console.log(`Show data fetched. Count: ${data.length}`)
+
+    let sudukuPuzzle = [1,2,3,4,5,6,7,8,9];
+    return {
+        sudoku:sudukuPuzzle
+    }
+}
+*/
+
+export default Sudoku;
