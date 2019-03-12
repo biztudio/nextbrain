82ee4a443ab74135ca13439de54ff0a3c9446d42
Yang SHEN <biztudio@outlook.com>
2019-03-12 14:14:50 +0800

    style the about

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,33 @@
 import Markdown from 'react-markdown';
 import Layout from '../components/Layout'
 
+//如果要让某个样式的优先级变高，可以使用!important来指定
 export default () => (
     
-    <Layout   title="关于 Nextbrain">
+    <Layout title="关于 Nextbrain">
 
-        <div className="markdown">
+        <div className="markdown aboutbox">
             <Markdown source={`
+![logo of biztudio](../static/logo_work002.png)
+
+
 ### This is Nextbrain
 
 *良好的专注力与思维来自于科学的训练.*
             `}/>           
         </div>
+
+        <style jsx global>{`
+            body{
+                background:#2CA3E7 !important;
+            }
+
+            .aboutbox{
+                display: flex;
+                flex-direction:column;
+                justify-content:flex-start;
+            }
+        `}</style>
 
     </Layout>
     
