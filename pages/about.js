baaac1f1bf647ba18a3d9ec59653e151cfeec0a5
Yang SHEN <biztudio@outlook.com>
2019-03-12 15:42:59 +0800

    style the about

--- pages/about.js
+++ pages/about.js
@@ -6,14 +6,23 @@ export default () => (
     
     <Layout title="关于 Nextbrain">
 
+        <div className='logobox'>
+            <div>
+                <img src='../static/logo_work002.png' />
+            </div>
+            <div>
+                基于敏捷的学习与实践工坊
+            </div>
+        </div>
+        
         <div className="markdown aboutbox">
             <Markdown source={`
-![logo of biztudio](../static/logo_work002.png)
-
 
 ### This is Nextbrain
 
-*良好的专注力与思维来自于科学的训练.*
+* 良好的专注力与思维来自于科学的刻意训练;
+* 学奥数，不如玩数独，无计算门槛的逻辑推理游戏；
+
             `}/>           
         </div>
 
@@ -22,10 +31,18 @@ export default () => (
                 background:#2CA3E7 !important;
             }
 
+            .logobox{
+                width:100%;
+                display:flex;
+                flex-direction:column;
+                align-items: flex-end;
+            }
+
             .aboutbox{
                 display: flex;
                 flex-direction:column;
                 justify-content:flex-start;
+                margin-top:35px;
             }
         `}</style>
 
