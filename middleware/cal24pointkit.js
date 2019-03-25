3a886daa52c40ae76aa5e81eec34e42935f8bb5d
Yang SHEN <biztudio@outlook.com>
2019-03-25 14:28:04 +0800

    add feature & test for cal24kit.

--- middleware/cal24pointkit.js
+++ middleware/cal24pointkit.js
@@ -1,4 +1,5 @@
 import mathkit from './mathkit';
+import * as math from 'mathjs';
 
 const patterns = [
     'heart',//红桃
@@ -7,6 +8,72 @@ const patterns = [
     'diamond'//方片
 ];
 
+const EXPECT_RESULT = 24;
+
+const recursive2Parts = (digits, targetNumber) => {
+        
+    let checkResult = false;
+    if(digits.length == 1){
+        checkResult = digits[0] == targetNumber;
+    }
+    else{
+        
+        for(let index1 = 0; index1 < digits.length - 1; index1++){
+            for(let index2 = index1 + 1; index2 < digits.length; index2++){
+
+                let curDigit1 = digits[index1];
+                let curDigit2 = digits[index2];
+
+                let restDigits = [];
+                for(let di = 0; di < digits.length; di++){
+                    if(di != index1 && di != index2) {
+                        restDigits.push(digits[di]);
+                    }
+                }
+
+                if(!checkResult){
+                    restDigits.push((curDigit1 + curDigit2));
+                    checkResult = checkResult || recursive2Parts(restDigits.slice(0), targetNumber);
+                    restDigits.pop();
+                }
+        
+                if(!checkResult){
+                    restDigits.push((curDigit1 * curDigit2));
+                    checkResult = checkResult || recursive2Parts(restDigits.slice(0), targetNumber);
+                    restDigits.pop();
+                }
+        
+                if(!checkResult){
+                    restDigits.push((curDigit1 - curDigit2));
+                    checkResult = checkResult || recursive2Parts(restDigits.slice(0), targetNumber);
+                    restDigits.pop();
+                }
+        
+                if(!checkResult){
+                    restDigits.push((curDigit2 - curDigit1));
+                    checkResult = checkResult || recursive2Parts(restDigits.slice(0), targetNumber);
+                    restDigits.pop();
+                }
+        
+                if(!checkResult && curDigit2 != 0){
+                    restDigits.push(math.fraction(curDigit1 / curDigit2));
+                    checkResult = checkResult || recursive2Parts(restDigits.slice(0), targetNumber);
+                    restDigits.pop();
+                }
+        
+                if(!checkResult && curDigit1 != 0){
+                    restDigits.push(math.fraction(curDigit2 / curDigit1));
+                    checkResult = checkResult || recursive2Parts(restDigits.slice(0), targetNumber);
+                    restDigits.pop();
+                }
+
+            }
+        }
+    }
+    
+    return checkResult;
+};
+
 export default{
     getPuzzle(level){//level between 2 and 5
         let puzzle = {puzzleNumbers:[]};
@@ -14,9 +81,26 @@ export default{
             
         }
         return puzzle;
+    },
+
+    determine4DigitsMeet24Point(digits){
+        if(!digits || digits.length < 1) return false;
+
+        let calresult = 0;
+        calresult = digits.reduce((total, n) => total+n);
+        if(EXPECT_RESULT == calresult) return true;
+
+        calresult = 0;
+        calresult = digits.reduce((total, n) => total*n);
+        if(EXPECT_RESULT == calresult) return true;
+
+        calresult = 0;
+        let checkFlag = false;
+        checkFlag = checkFlag || recursive2Parts(digits, EXPECT_RESULT);
+
+        return checkFlag;
     }
 }
-
 
 //Refer 
 //https://blog.csdn.net/qq_40938169/article/details/82453743
--- test/cal24pointkit.test.js
+++ test/cal24pointkit.test.js
@@ -1,16 +1,81 @@
-//import React, { Component } from 'react';
-import cal24pointkit from '../middleware/cal24pointkit';
+import checker4Digits from '../middleware/cal24pointkit';
 
-/**/
-test('test level 2 puzzle length', () =>{
-    let result1 = cal24pointkit.getPuzzle(2).puzzleNumbers.length;
-    expect(result1).toBe(2);
-   
+describe('Test verification fucntion', () => {
+    test('[1,3,4,6] => true: 6 / (1 - 3/4) = 24', () => {
+        let digits =  [1,3,4,6];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeTruthy();
+    })
+
+    test('[4, 1, 8, 7] => true: (8-4) * (7-1) = 24', () => {
+        let digits =  [4, 1, 8, 7];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeTruthy();
+    })
+
+    test('[1, 4, 7, 8] => true: (8-4) * (7-1) = 24', () => {
+        let digits =  [1, 4, 7, 8];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeTruthy();
+    })
+
+    test('[1, 5, 7, 9] => true: (9-5) * (7-1) = 24', () => {
+        let digits =  [1, 5, 7, 9];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeTruthy();
+    })
+
+    test('[1, 4, 5, 9] => true: (4-1) * 5 + 9 = 24', () => {
+        let digits =  [1, 4, 5, 9];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeTruthy();
+    })
+
+    test('[1, 2, 1, 2] => false', () => {
+        let digits =  [1, 2, 1, 2];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeFalsy();
+    })
+
+    test('[1,2,3,4] => true: 1*2*3*4 = 24', () => {
+        let digits =  [1,2,3,4];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeTruthy();
+    })
+
+    test('[1,9,1,2] => true: (9-1) * (1+2) = 24', () => {
+        let digits =  [1,9,1,2];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeTruthy();
+    })
+
+    test('[3,3,8,8] => true: 8/(3-(8/3)) = 24', () => {
+        let digits =  [3,3,8,8];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeTruthy();
+    })
+
+    test('[3,8] => true: 3 * 8 = 24', () => {
+        let digits =  [3,8];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeTruthy();
+    })
+
+    test('[1,2,3,4,4] => true: (4+4)*3*(2-1) = 24', () => {
+        let digits =  [1,2,3,4,4];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeTruthy();
+    })
+
 });
 
 
-test('test verification', ()=>{
-    let result1 = 1;
-    let result2 = 1;
-    expect(result1).toBe(result2);
+describe('Test generator fucntion', () => {
+
+    test('[1,2,3,4,4] => true: (4+4)*3*(2-1) = 24', () => {
+        let digits =  [1,2,3,4,4];
+        let result = checker4Digits.determine4DigitsMeet24Point(digits);
+        expect(result).toBeTruthy();
+    })
+
 });
